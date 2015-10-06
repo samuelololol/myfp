a1135f48005f918df288af1e44db6a481b3c2c5e
samuelololol <samuelololol@gmail.com>
2015-10-06 23:10:27 +0800

    class

---
+++ class/README.md
@@ -0,0 +1,4 @@
+
+Another example of succinctness in Scala.
+`var` prefix to each parameter makes the Scala compiler generate a getter and
+setter for each field in the class.
---
+++ class/programmer.scala
@@ -0,0 +1,34 @@
+// JAVA: POJO(plain old Java object)
+// =================================
+//public class Programmer {
+//    private String name;
+//    private String language;
+//    private String favDrink;
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name){
+//        this.name = name;
+//    }
+//
+//    public String getLanguage() {
+//        return language;
+//    }
+//
+//    public void setLanguage(String language){
+//        this.language = language;
+//    }
+//
+//    public String getFavDrink() {
+//        return favDrink;
+//    }
+//
+//    public void setFavDrink(String favDrink){
+//        this.favDrink = favDrink;
+//    }
+//}
+
+// Scala with primary constructor
+class Programmer(var name:String, var language:String, var favDrink:String)
